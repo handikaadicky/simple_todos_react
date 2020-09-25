27801bd218e53bada917cd94289c0c70d7c85815
handikaadicky <handikaadicky@gmail.com>
2020-09-25 23:43:19 +0700

    add, delete with useState hooks

---
+++ src/Components/InputTodo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function InputTodo() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoChange = (env) => {
+    env.preventDefault();
+    setNewTodo(env.target.value);
+  };
+
+  const handleNewTodo = (env) => {
+    env.preventDefault();
+    if (newTodo === "") return;
+    setTodos([...todos, { id: Date.now(), text: newTodo }]);
+    env.target.reset();
+  };
+
+  const removeTodo = (id) => {
+    todos.filter((todo) => todo.id === id);
+  };
+
+  console.log(todos);
+  console.log(newTodo);
+
+  return (
+    <div>
+      <form onSubmit={handleNewTodo}>
+        <input placeholder="Todos..." onChange={handleNewTodoChange}></input>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button onClick={() => removeTodo(todo.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
+}
+
+export default InputTodo;
